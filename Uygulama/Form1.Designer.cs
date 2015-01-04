0af16bdf4d20b2e6ecdc0818dd77e23b144a039f
nbky <nebikaya22@hotmail.com>
2015-01-04 16:42:23 +0200

    Derste yapılan son form uygulaması butonlara  basınca kırmızı oluyordu aynı zamanda texboxa uzerınde rakamı eklıyordu.

---
+++ Uygulama/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace Uygulama
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.sayi1 = new System.Windows.Forms.Button();
+            this.sayi2 = new System.Windows.Forms.Button();
+            this.sayi3 = new System.Windows.Forms.Button();
+            this.sayi6 = new System.Windows.Forms.Button();
+            this.sayi5 = new System.Windows.Forms.Button();
+            this.sayi4 = new System.Windows.Forms.Button();
+            this.sayi9 = new System.Windows.Forms.Button();
+            this.sayi8 = new System.Windows.Forms.Button();
+            this.sayi7 = new System.Windows.Forms.Button();
+            this.sayi0 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // sayi1
+            // 
+            this.sayi1.Font = new System.Drawing.Font("Calibri Light", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.sayi1.Location = new System.Drawing.Point(30, 120);
+            this.sayi1.Name = "sayi1";
+            this.sayi1.Size = new System.Drawing.Size(60, 60);
+            this.sayi1.TabIndex = 1;
+            this.sayi1.Text = "1";
+            this.sayi1.UseVisualStyleBackColor = true;
+            // 
+            // sayi2
+            // 
+            this.sayi2.Font = new System.Drawing.Font("Calibri Light", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.sayi2.Location = new System.Drawing.Point(120, 120);
+            this.sayi2.Name = "sayi2";
+            this.sayi2.Size = new System.Drawing.Size(60, 60);
+            this.sayi2.TabIndex = 2;
+            this.sayi2.Text = "2";
+            this.sayi2.UseVisualStyleBackColor = true;
+            // 
+            // sayi3
+            // 
+            this.sayi3.Font = new System.Drawing.Font("Calibri Light", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.sayi3.Location = new System.Drawing.Point(210, 120);
+            this.sayi3.Name = "sayi3";
+            this.sayi3.Size = new System.Drawing.Size(60, 60);
+            this.sayi3.TabIndex = 3;
+            this.sayi3.Text = "3";
+            this.sayi3.UseVisualStyleBackColor = true;
+            // 
+            // sayi6
+            // 
+            this.sayi6.Font = new System.Drawing.Font("Calibri Light", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.sayi6.Location = new System.Drawing.Point(210, 210);
+            this.sayi6.Name = "sayi6";
+            this.sayi6.Size = new System.Drawing.Size(60, 60);
+            this.sayi6.TabIndex = 6;
+            this.sayi6.Text = "6";
+            this.sayi6.UseVisualStyleBackColor = true;
+            // 
+            // sayi5
+            // 
+            this.sayi5.Font = new System.Drawing.Font("Calibri Light", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.sayi5.Location = new System.Drawing.Point(120, 210);
+            this.sayi5.Name = "sayi5";
+            this.sayi5.Size = new System.Drawing.Size(60, 60);
+            this.sayi5.TabIndex = 5;
+            this.sayi5.Text = "5";
+            this.sayi5.UseVisualStyleBackColor = true;
+            // 
+            // sayi4
+            // 
+            this.sayi4.Font = new System.Drawing.Font("Calibri Light", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.sayi4.Location = new System.Drawing.Point(30, 210);
+            this.sayi4.Name = "sayi4";
+            this.sayi4.Size = new System.Drawing.Size(60, 60);
+            this.sayi4.TabIndex = 4;
+            this.sayi4.Text = "4";
+            this.sayi4.UseVisualStyleBackColor = true;
+            // 
+            // sayi9
+            // 
+            this.sayi9.Font = new System.Drawing.Font("Calibri Light", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.sayi9.Location = new System.Drawing.Point(210, 300);
+            this.sayi9.Name = "sayi9";
+            this.sayi9.Size = new System.Drawing.Size(60, 60);
+            this.sayi9.TabIndex = 9;
+            this.sayi9.Text = "9";
+            this.sayi9.UseVisualStyleBackColor = true;
+            // 
+            // sayi8
+            // 
+            this.sayi8.Font = new System.Drawing.Font("Calibri Light", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.sayi8.Location = new System.Drawing.Point(120, 300);
+            this.sayi8.Name = "sayi8";
+            this.sayi8.Size = new System.Drawing.Size(60, 60);
+            this.sayi8.TabIndex = 8;
+            this.sayi8.Text = "8";
+            this.sayi8.UseVisualStyleBackColor = true;
+            // 
+            // sayi7
+            // 
+            this.sayi7.Font = new System.Drawing.Font("Calibri Light", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.sayi7.Location = new System.Drawing.Point(30, 300);
+            this.sayi7.Name = "sayi7";
+            this.sayi7.Size = new System.Drawing.Size(60, 60);
+            this.sayi7.TabIndex = 7;
+            this.sayi7.Text = "7";
+            this.sayi7.UseVisualStyleBackColor = true;
+            // 
+            // sayi0
+            // 
+            this.sayi0.Font = new System.Drawing.Font("Calibri Light", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.sayi0.Location = new System.Drawing.Point(120, 390);
+            this.sayi0.Name = "sayi0";
+            this.sayi0.Size = new System.Drawing.Size(60, 60);
+            this.sayi0.TabIndex = 0;
+            this.sayi0.Text = "0";
+            this.sayi0.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Font = new System.Drawing.Font("Calibri Light", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox1.Location = new System.Drawing.Point(30, 36);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(240, 56);
+            this.textBox1.TabIndex = 0;
+
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(304, 473);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.sayi0);
+            this.Controls.Add(this.sayi9);
+            this.Controls.Add(this.sayi8);
+            this.Controls.Add(this.sayi7);
+            this.Controls.Add(this.sayi6);
+            this.Controls.Add(this.sayi5);
+            this.Controls.Add(this.sayi4);
+            this.Controls.Add(this.sayi3);
+            this.Controls.Add(this.sayi2);
+            this.Controls.Add(this.sayi1);
+            this.Name = "Form1";
+            this.Text = "Uygulama";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button sayi1;
+        private System.Windows.Forms.Button sayi2;
+        private System.Windows.Forms.Button sayi3;
+        private System.Windows.Forms.Button sayi6;
+        private System.Windows.Forms.Button sayi5;
+        private System.Windows.Forms.Button sayi4;
+        private System.Windows.Forms.Button sayi9;
+        private System.Windows.Forms.Button sayi8;
+        private System.Windows.Forms.Button sayi7;
+        private System.Windows.Forms.Button sayi0;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Uygulama/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Uygulama
+{
+    public partial class Form1 : Form
+    {
+        /* Form üzerindeki bütün butonları bu listede toplayacağız.*/
+        List<Button> FormdakiButonlar = new List<Button>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            FormdakiButonlar.Clear();
+            /* Tüm butonları listeye ekliyoruz */
+            foreach (Control eleman in this.Controls)
+            {
+                if (eleman is Button)
+                {
+                    FormdakiButonlar.Add(eleman as Button);
+                }
+            }
+
+            for (int i = 0; i < FormdakiButonlar.Count; i++)
+            {
+                FormdakiButonlar[i].Click += new EventHandler(Klik);
+            }
+        }
+
+        void KlikCikar(string klik)
+        {
+            string text = "";
+            for (int i = 0; i < textBox1.Text.Length; i++)
+            {
+                if (textBox1.Text[i] != Convert.ToChar(klik))
+                {
+                    text += Convert.ToString(textBox1.Text[i]);    
+                }
+            }
+            textBox1.Text = text;
+
+        }
+
+        void Klik(object sender, EventArgs e) 
+        {
+            Button TiklananButon = sender as Button;
+
+            if (textBox1.Text.IndexOf(Convert.ToChar(TiklananButon.Text), 0) >= 0)
+            {
+                KlikCikar(TiklananButon.Text);
+                TiklananButon.UseVisualStyleBackColor = true;
+            }
+            else
+            {
+                textBox1.Text += TiklananButon.Text;
+                TiklananButon.BackColor = Color.Red;
+                textBox1.Focus();
+                textBox1.SelectionStart = textBox1.Text.Length;
+            }
+        }
+
+        
+        
+
+        
+       
+    }
+}
